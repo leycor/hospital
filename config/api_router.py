e57a33f04c98d9467098f63eada14e2d9bc144de
Bryan Rivas <bryanjohann1995@gmail.com>
2020-04-13 23:36:33 -0400

    Módulo hospital finalizado, ahora si =)

--- config/api_router.py
+++ config/api_router.py
@@ -1,14 +1,14 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from hospital.users.api.views import UserViewSet
+from hospital.users.api import views
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-router.register("users", UserViewSet)
+router.register("hospitals", views.HospitalUserViewSet, basename="hospitals")
 
 
 app_name = "api"
