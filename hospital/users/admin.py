22ec2f32e8aea7dae2a5ec745de75d7d484cb94a
Bryan Rivas <bryanjohann1995@gmail.com>
2020-04-13 20:53:12 -0400

    Modelo de super usuario Hospital, finalizado

--- hospital/users/admin.py
+++ hospital/users/admin.py
@@ -1,17 +1,13 @@
+#DJANGO
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
-from django.contrib.auth import get_user_model
 
-from hospital.users.forms import UserChangeForm, UserCreationForm
-
-User = get_user_model()
+#MODELS
+from hospital.users.models import User
 
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
-
-    form = UserChangeForm
-    add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "name", "is_superuser"]
-    search_fields = ["name"]
+    readonly_fields = ('created','updated')
+    list_display = ['username','is_doctor','is_admin','is_staff','name']
+    list_filter = ['is_staff','is_admin','is_doctor']
---
+++ hospital/users/models/user.py
@@ -0,0 +1,22 @@
+#DJANGO
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+#UTILS
+from hospital.utils.models import HospitalModel
+
+
+class User(HospitalModel, AbstractUser):
+
+    name = models.CharField('hospital name',max_length=200, blank=False, null=False)
+    direction = models.CharField(max_length=200, blank=False, null=False)
+    phone = models.CharField(max_length=15)
+    is_doctor = models.BooleanField('doctor status',default=False)
+    is_admin = models.BooleanField('admin status',default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name','direction','phone','password']
+
+    def __str__(self):
+        return self.self.username
+    
